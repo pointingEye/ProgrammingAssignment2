7828d35d7a62e32ee32b3f5dcedbd3cfba8cf66d
pointingEye <g@juliushassemer.de>
2014-10-22 09:26:29 +0200

    Update cachematrix.R
    describing what the functions do

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## overall
+# the following two functions are an adaptation of the mean calculation and caching of a vector by RD Peng that allow to calculate the inversion of a matrix using a prior cached inverse matrix when possible
 
-## Write a short comment describing this function
+## cache function
+# this function caches the matrix to be inverted and the result of the matrix inversion, potentially releaving the function after this one of the necessety to recalculate an already perfromed inversion. It allows to get and set the matrix and to set and get the inverse matrix.
+# not that I continue to use m, but now standing for matrix, not for mean
 
 makeCacheMatrix <- function(x = numeric()) {
   m <- NULL
@@ -17,7 +19,7 @@ makeCacheMatrix <- function(x = numeric()) {
        getImatrix = getImatrix)
 }
 
-## Write a short comment describing this function
+## function that inverts the matrix, but takes the already inverted matrix if cached by the function above
 
 cacheSolve <- function(x, ...) {
   m <- x$getImatrix()
